39a861142896a13571ca859fba78df7e86793461
SakuraNotStupid <sakurakyuo@protonmail.com>
2023-09-09 17:11:37 +0800

    Camera: clone Google Camera when build
    Change-Id: Id12c2a065e79c80af9ca0f1c199f7c9ef18a76d5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,10 +3,10 @@ rm -rf device/xiaomi/sdm710-common
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf hardware/xiaomi
-rm -rf packages/apps/GCamGOPrebuilt
+rm -rf device/xiaomi/grus/prebuilt/GoogleCameraENG
 # clone
 git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common
 git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi
 git clone --depth=1 https://github.com/SakuraNotStupid/android_kernel_xiaomi_sdm710 kernel/xiaomi/sdm710
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt -b arrow-13.1 packages/apps/GCamGOPrebuilt
+git clone https://gitlab.com/SakuraNotStupid/GoogleCameraENG.git device/xiaomi/grus/prebuilt/GoogleCameraENG
