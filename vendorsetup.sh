854c7da7dba66ed275fb731057f4639370879111
SakuraKyuo <sakurakyuo@protonmail.com>
2023-11-18 08:16:34 +0800

    grus: update vendorsetup.sh
    Change-Id: I4cd7c4a9a253711356d6ea6d4a5649f8543f4418

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,8 +5,8 @@ rm -rf kernel/xiaomi
 rm -rf hardware/xiaomi
 rm -rf device/xiaomi/grus/prebuilt/GoogleCameraENG
 # clone
-git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common -b Tiramisu
-git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b Tiramisu
-git clone --depth=1 https://github.com/SakuraNotStupid/android_kernel_xiaomi_sdm710 kernel/xiaomi/sdm710 -b Tiramisu
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common -b UDC
+git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b UDC
+git clone --depth=1 https://github.com/SakuraNotStupid/android_kernel_xiaomi_sdm710 kernel/xiaomi/sdm710 -b UDC
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 git clone https://gitlab.com/SakuraNotStupid/GoogleCameraENG.git device/xiaomi/grus/prebuilt/GoogleCameraENG
