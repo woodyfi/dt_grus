7283bcc79ca7ffef4edd5ea3f3f72ecee5cc2b11
Sakura桜 <63789876+SakuraKyuo@users.noreply.github.com>
2024-05-10 17:24:14 +0800

    grus: Test dolby Atmos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf hardware/xiaomi
 # clone
-git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common -b UDC
-git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b UDC
+git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common -b UDC_Dolby
+git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b UDC_Dolby
 git clone --depth=1 https://github.com/SakuraKyuo/android_kernel_xiaomi_sdm710-twilight kernel/xiaomi/sdm710 -b 14
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
