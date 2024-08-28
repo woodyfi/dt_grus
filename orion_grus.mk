59d4b66a78f5c13b57c9fd3e8ed04faf928c7dee
ELbee <85691313+woodyfi@users.noreply.github.com>
2024-08-28 23:23:14 +0700

    Update and rename afterlife_grus.mk to orion_grus.mk

--- orion_grus.mk
+++ orion_grus.mk
@@ -9,22 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Elixir stuff.
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Afterlife stuff
-AFTERLIFE_BUILD_TYPE := OFFICIAL
-AFTERLIFE_MAINTAINER := sweetbtrfly
-
-# Device config
-TARGET_HAS_UDFPS := true
+ORION_MAINTAINER := Ph8+
+ORION_MAINTAINER_LINK := https://t.me/PH8BUILD
+ORION_BUILD_TYPE := UNOFFICIAL
+ORION_GAPPS := false
 TARGET_ENABLE_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS :=  true
+
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
 
 # Inherit from grus device
 $(call inherit-product, device/xiaomi/grus/device.mk)
 
-PRODUCT_NAME := afterlife_grus
+PRODUCT_NAME := orion_grus
 PRODUCT_DEVICE := grus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
